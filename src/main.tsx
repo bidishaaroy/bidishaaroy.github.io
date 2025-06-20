1236056e619dc3ca8da1bd4607fba556625b7aa7
bidisha <bidisharoybrn@gmail.com>
2025-06-19 22:52:56 -0700

    fixed errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
+
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { ThemeProvider } from 'next-themes'    
+import App from './App'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById('root')!
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,22 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react       from '@vitejs/plugin-react-swc'
+import path        from 'path'
 
+export default defineConfig({
+  // for a user site (yourname.github.io) this should be "/"
+  base: '/',
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/bidishaaroy.github.io/' : '/',
-  server: {
-    host: "::",
-    port: 8080,
+  build: {
+    // GH-Pages will serve files out of /docs on the main branch
+    outDir: 'docs',
   },
-  plugins: [
-    react(),
-  ],
+
+  plugins: [react()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-  },
-}));
+})

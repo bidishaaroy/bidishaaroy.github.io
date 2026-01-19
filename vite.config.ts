ef8540c56f11eb0ce5bd853c71adcf9f07ca2e2d
bidisha <bidisharoybrn@gmail.com>
2026-01-18 20:43:19 -0800

    Build site for GitHub Pages (docs)

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react       from '@vitejs/plugin-react-swc'
 import path        from 'path'
 
 export default defineConfig({
-  // for a user site (yourname.github.io) this should be "/"
+  
   base: '/',
 
   build: {
-    // GH-Pages will serve files out of /docs on the main branch
+   
     outDir: 'docs',
   },
 
